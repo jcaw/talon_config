7b49da76092493024fb0c96f5670ca2fb5b7997a
jcaw <40725916+jcaw@users.noreply.github.com>
2020-01-13 11:04:13 +0000

    Add sound player utility + basic sounds
    Allows us to play a few preset sound cues

---
+++ utils/sound.py
@@ -0,0 +1,56 @@
+import os
+import logging
+
+from ._wav import load_wav, play_wav
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+_sounds = "user/assets/sounds"
+
+BELL = f"{_sounds}/bike_bell_low.wav"
+BELL_HIGH = f"{_sounds}/bike_bell_high.wav"
+ERROR = f"{_sounds}/error.wav"
+FAILURE = f"{_sounds}/failure.wav"
+GLASS_TAP = f"{_sounds}/glass_tap.wav"
+TAP = f"{_sounds}/tap.wav"
+THUNK = f"{_sounds}/thunk.wav"
+WOOD_HIT = f"{_sounds}/wood_hit.wav"
+
+# Cache all the sound effects up-front to avoid a pause on first play.
+for path in [BELL, BELL_HIGH, ERROR, FAILURE, GLASS_TAP, TAP, THUNK, WOOD_HIT]:
+    try:
+        load_wav(path)
+    except Exception:
+        pass
+
+
+def play(path, ignore_missing_file=True):
+    """Play a sound.
+
+    :param bool ignore_missing_file: Optional. Catch error if the sound file is
+      missing. Default is True.
+
+    """
+    if not path.lower().endswith("wav"):
+        raise RuntimeError("Can only play wav files.")
+
+    if os.path.isfile(path):
+        play_wav(path)
+    elif ignore_missing_file:
+        LOGGER.error(f'`safe_play_sound`: file not found: "{path}".')
+    else:
+        raise IOError(f'Could not find sound file: "{path}"')
+
+
+# Some convenience methods to abstract the specific sound choice and make
+# the soundscape more consistent.
+def play_ding(**kwargs):
+    """Play a pleasant confirmation ding."""
+    play(BELL, **kwargs)
+
+
+def play_cancel(**kwargs):
+    """Play a gentle cancellation sound."""
+    play(FAILURE, **kwargs)
