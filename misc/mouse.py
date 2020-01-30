db2b76070704dc6f8c934cf1292ca3b88ed716a3
jcaw <40725916+jcaw@users.noreply.github.com>
2020-01-30 16:45:23 +0000

    Simplify calibrate command

--- misc/mouse.py
+++ misc/mouse.py
@@ -287,7 +287,7 @@ mouse_context.keymap(
         "(track | eye) mouse": toggle_eye_mouse_exclusive,
         "zoom mouse": toggle_zoom_mouse_exclusive,
         "camera overlay": lambda m: eye_mouse.camera_overlay.toggle(),
-        "run calibration": lambda m: eye_mouse.calib_start(),
+        "calibrate": lambda m: eye_mouse.calib_start(),
         # Clicks
         "click": click,
         "(rick | rickle | right click)": dynamic_action(right_click),
