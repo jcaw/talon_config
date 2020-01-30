8ce3cf4777ad785a1e8614b4985a958b961e071d
jcaw <40725916+jcaw@users.noreply.github.com>
2020-01-30 17:02:41 +0000

    Add meta-function that can map multiple strings to one value

--- utils/__init__.py
+++ utils/__init__.py
@@ -229,3 +229,25 @@ def dictify(list_):
 
     """
     return {element: element for element in list_}
+
+
+def multi_map(mapping):
+    """Meta-function to allow multiple strings to be mapped to one value.
+
+    The input `mapping` will be expanded like this:
+
+    >>> multi_map({"a": "1", ("b", "c", "d"): "2"})
+    {"a": "1", "b": "2", "c": "2", "d": "2"}
+
+    (Note we use a tuple as the key because dictionary keys must be hashable).
+
+    """
+    result = {}
+    for key, value in mapping.items():
+        if isinstance(key, tuple):
+            # Map each list element individually
+            for element in key:
+                result[element] = value
+        else:
+            result[key] = value
+    return result
