7cc0cc5756317890f7d9a9354327da66498fe805
jcaw <40725916+jcaw@users.noreply.github.com>
2020-02-02 19:12:48 +0000

    Add documentation

--- misc/mouse.py
+++ misc/mouse.py
@@ -331,6 +331,7 @@ def delayed_drop(m=None):
 
 
 def queue_drag_drop(m):
+    """Queue a drag, plus the subsequent drop, on the zoom mouse."""
     queue_zoom_action(drag)(m)
     # Add drop on a delay so the ding rings twice.
     cron.after("150ms", lambda: queue_zoom_action(delayed_drop)(m))
