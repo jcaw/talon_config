73f43b0c5a84972c4ee504accf6d0fd1b87dc9c0
jcaw <40725916+jcaw@users.noreply.github.com>
2020-02-28 11:29:04 +0000

    Shorter YouTube content regexp

--- apps/websites/youtube.py
+++ apps/websites/youtube.py
@@ -9,7 +9,7 @@ from ._web_context import WebContext
 #
 # HACK: Talon can't seem to get the title when YouTube is fullscreen - it's
 #   just empty. Activate with all empty firefox titles.
-context = WebContext("youtube", r"( \- YouTube$)|(^$)")
+context = WebContext("youtube", r"(\- YouTube$)|(^$)")
 
 
 def reset_video_speed(m):
