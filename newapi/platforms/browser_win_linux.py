49b0261e6a88e9faabc788cefceb20b9c877603a
jcaw <40725916+jcaw@users.noreply.github.com>
2020-03-08 13:26:01 +0000

    Add browser commands for [windows] & [linux] (first draft)
    This is just a first draft - they're aimed at Firefox, not significantly tested
    yet, not tested on Linux at all. Problems will be fixed as they crop up.

---
+++ newapi/platforms/browser_win_linux.py
@@ -0,0 +1,137 @@
+"""Default browser settings for Windows and Linux.
+
+Written for Firefox. Some of these will need to be overridden in specific
+browsers.
+
+NOTE: These are UNTESTED on Linux. Some are definitely wrong.
+
+TODO: Audit on Linux
+
+"""
+
+import time
+
+from talon import Context, actions
+
+key = actions.key
+
+
+context = Context()
+context.matches = r"""
+os: windows
+os: linux
+
+app: firefox
+app: /.*firefox\.exe/
+app: chrome
+app: /.*chrome\.exe/
+# NOTE: Doesn't work properly with Internet Explorer. Will need its own module.
+app: /.*iexplore\.exe/
+app: opera
+
+# TODO: Compatibility with Edge?
+# title: /.*- Microsoft Edge/
+"""
+
+
+@context.action_class("app")
+class AppActions:
+    def tab_close():
+        key("ctrl-w")
+
+    def tab_next():
+        key("ctrl-tab")
+
+    def tab_previous():
+        key("ctrl-shift-tab")
+
+    def tab_open():
+        key("ctrl-t")
+
+    def tab_reopen():
+        key("ctrl-shift-t")
+
+
+@context.action_class("browser")
+class BrowserActions:
+    # TODO: Talon seems to register a pre:phrase hook that triggers this,
+    # focussing the bar. Don't want that, so remove for now. Probably have to
+    # use APIs or extensions (or maybe accessibility interfaces)
+    #
+    # def address():
+    #     actions.browser.focus_address()
+    #     time.sleep(0.1)
+    #     actions.edit.copy()
+    #     actions.browser.focus_page()
+
+    def bookmark():
+        key("ctrl-d")
+
+    def bookmark_tabs():
+        key("ctrl-shift-d")
+
+    # TODO: Individual implementations
+    # def bookmarks():
+    #     key("")
+
+    def bookmarks_bar():
+        key("ctrl-b")
+
+    def focus_address():
+        key("ctrl-l")
+
+    def focus_page():
+        # TODO: Firefox only?
+        key("shift-f6")
+
+    def focus_search():
+        key("ctrl-e")
+
+    # def go():
+    #     raise NotImplementedError()
+
+    def go_back():
+        key("alt-left")
+
+    def go_forward():
+        key("alt-right")
+
+    # def go_blank():
+    #     raise NotImplementedError()
+
+    def go_home():
+        key("alt-home")
+
+    def open_private_window():
+        key("ctrl-shift-p")
+
+    def reload():
+        key("f5")
+
+    # def reload_hard():
+    #     raise NotImplemetedError()
+
+    def reload_hardest():
+        key("ctrl-f5")
+
+    def show_clear_cache():
+        key("ctrl-shift-delete")
+
+    def show_downloads():
+        # TODO: Different on Linux
+        key("ctrl-j")
+
+    # def show_extensions():
+    #     raise NotImplementedError()
+
+    def show_history():
+        key("ctrl-h")
+
+    # def submit_form():
+    #     raise NotImplementedError()
+
+    # def title():
+    #     raise NotImplementedError()
+
+    def toggle_dev_tools():
+        key("f12")
---
+++ talon_files/browser.talon
@@ -0,0 +1,47 @@
+# TODO: Switch to tags
+
+app: firefox
+app: /.*firefox\.exe/
+app: chrome
+app: /.*chrome\.exe/
+app: /.*iexplore\.exe/
+app: /.*edge\.exe/
+app: opera
+-
+# Browser GUI
+# TODO: Optional dictation
+address [bar]: browser.focus_address()
+# TODO: Optional dictation
+search [bar]: browser.focus_search()
+[focus] page: browser.focus_page()
+# TODO: Copy address?
+# TODO: Copy page title?
+
+
+# Navigation
+go back: browser.go_back()
+go forward: browser.go_forward()
+go home | homepage: browser.go_home()
+go blank [page]: browser.go_blank()
+(refresh | reload) [light]: browser.reload()
+(refresh | reload) medium: browser.reload_hard()
+(refresh | reload) hard: browser.reload_hardest()
+# TODO: Go to a page, browser.go() - should probably be global
+
+
+# Bookmarks, History, etc.
+(bookmark page | add bookmark): browser.bookmark()
+bookmark (all | tabs | all tabs): browser.bookmark_tabs()
+# Bookmarks & bookmarks bar are different
+show bookmarks: browser.bookmarks()
+bookmarks bar: browser.bookmarks_bar()
+[show] history [bar]: browser.show_history()
+[show] (downloads | download) [bar]: browser.show_downloads()
+[show] extensions: browser.show_extensions()
+(clear | delete) (cache | cached data): browser.show_clear_cache()
+
+
+# Unsorted
+[open] private [(window | browsing)]: browser.open_private_window()
+(dev | developer) tools: browser.toggle_dev_tools()
+submit [form]: browser.submit_form()
