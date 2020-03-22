272d0a9e0f1b280965965a6de612f5f7388f4427
jcaw <40725916+jcaw@users.noreply.github.com>
2020-03-22 12:44:23 +0000

    Port input switching module (uses PyPI keyboard) to newapi

--- newapi/games/utils/switch_input.py
+++ newapi/games/utils/switch_input.py
@@ -4,9 +4,11 @@ Replaces Talon's native input within certain contexts.
 
 """
 
+import time
 
-def switch_to_keyboard(context):
-    """Switch to using the `keyboard` module for input in this context.
+
+def switch_to_keyboard_module(context):
+    """Switch to using the PyPI `keyboard` module for input in this context.
 
     Some games aren't registering Talon's input as of Windows beta 988 (for
     example, Europa Universalis IV). However, the PyPi `keyboard` module seems
@@ -18,6 +20,9 @@ def switch_to_keyboard(context):
     @context.action_class("main")
     class MainActions:
         def key(key: str):
-            keys = key.split("-")
-            assert len(keys) > 0
-            keyboard.press_and_release("+".join(keys))
+            # Naive method - this may not cover all keys.
+            for individual_press in key.split(" "):
+                keys = individual_press.split("-")
+                keyboard.press_and_release("+".join(keys))
+                # TODO: Integrate key settings, switch this delay
+                time.sleep(0.02)
