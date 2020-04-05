2c36c1a51137bc0b758cccb037f0e8e5b710475e
jcaw <40725916+jcaw@users.noreply.github.com>
2020-04-05 13:51:15 +0100

    Emacs Flycheck auto-generated files interfering, disable it
    Disable flycheck when opening python files in this project in Emacs. See comment
    for details.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
 ((nil . (
          ))
  (python-mode . ((eval . (blacken-mode 1))
+                 ;; Some backends cause Flycheck to create temp .py files. Talon
+                 ;; will try and auto-load them, causing problems. Blunt
+                 ;; solution is to disable it outright.
+                 (eval . (flycheck-mode 0))
                  )))
