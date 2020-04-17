e77ec5432d89cbe2724d84e41bb3d7c9ed05bd11
jcaw <40725916+jcaw@users.noreply.github.com>
2020-04-17 18:28:59 +0100

    In Magit, use relative line numbers for mark multiple lines
    Don't treat the number as a repeat by shaving 1 off it

--- emacs/magit.py
+++ emacs/magit.py
@@ -9,4 +9,6 @@ class Actions:
     def magit_mark_lines(number: int) -> None:
         """Mark a specific number of lines in magit."""
         actions.user.emacs_command("set-mark-command")
-        actions.key("down:{}".format(number - 1))
+        # This will actually mark (number + 1) lines, but it means we can read
+        # directly off the relative line numbers.
+        actions.key("down:{}".format(number))
