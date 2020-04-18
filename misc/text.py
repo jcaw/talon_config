1c7620580907fb638a039f5df46cdb4224c8a04c
jcaw <40725916+jcaw@users.noreply.github.com>
2020-04-18 18:23:20 +0100

    Pass formatters as strings for talon file compatibility
    In order to use formatters in a .talon file, pass them as a list of
    space-separated strings. (This is separate to the voice bindings, which are
    defined in their own list and map to these string names.)

--- misc/text.py
+++ misc/text.py
@@ -31,28 +31,50 @@ from user.utils import multi_map, preserve_clipboard
 # TODO: Insert punctuation, like brackets.
 
 
+formatter_functions = {
+    "camel": apply_camel_case,
+    "studley": apply_studley_case,
+    "snake": apply_snake,
+    "spine": apply_spine,
+    "dotword": apply_dotword,
+    "squash": apply_squash,
+    "c_path": make_apply_delimiter("::"),
+    "dunder": apply_dunder,
+    "uppercase": apply_uppercase,
+    "lowercase": apply_lowercase,
+    "sentence": apply_sentence,
+    "capitalized_sentence": apply_capitalized_sentence,
+    "title": apply_title,
+    "keywords": apply_programming_keywords,
+}
+
+
 # TODO: Separate atomic modifiers that can't be chained, e.g. Natural Language?
 #   Own map, don't even allow chaining?
-formatter_map = multi_map(
+_spoken_formatters = multi_map(
     {
-        "camel": apply_camel_case,
-        "studley": apply_studley_case,
-        "snake": apply_snake,
-        "spine": apply_spine,
+        "camel": "camel",
+        "studley": "studley",
+        "snake": "snake",
+        "spine": "spine",
         # TODO: Maybe change this command. Dotty?
-        "dotword": apply_dotword,
-        "squash": apply_squash,
+        "dotword": "dotword",
+        "squash": "squash",
         # TODO: Where is this delimiter used again?
-        "see path": make_apply_delimiter("::"),  # c path
-        "dunder": apply_dunder,
-        "upper": apply_uppercase,
-        ("lower", "bot"): apply_lowercase,
-        "say": apply_sentence,
-        "top": apply_capitalized_sentence,
-        "title": apply_title,
-        "key": apply_programming_keywords,
+        "see path": "c_path",  # c path
+        "dunder": "dunder",
+        "upper": "uppercase",
+        ("lower", "bot"): "lowercase",
+        "say": "sentence",
+        "top": "capitalized_sentence",
+        "title": "title",
+        "key": "keywords",
     }
 )
+
+# Sanity check
+for spoken_form, formatter_name in _spoken_formatters.items():
+    assert formatter_name in formatter_functions
 
 
 # TODO: Wrapped, e.g. parens and string?
@@ -65,12 +87,12 @@ formatter_map = multi_map(
 
 
 module = Module()
-module.list("formatters", desc="List of formatters")
+module.list("formatters", desc="List of basic text formatters")
 
 
 @module.capture
-def formatters(m) -> List[str]:
-    """List of text formatters."""
+def formatters(m) -> str:
+    """One or more text formatters, as a space-separated string."""
 
 
 @module.capture(rule="<phrase>")
@@ -80,7 +102,7 @@ def dictation(m) -> str:
 
 
 context = Context()
-context.lists["self.formatters"] = formatter_map.keys()
+context.lists["self.formatters"] = _spoken_formatters
 
 
 def join_punctuation(words: List[str]) -> str:
@@ -94,16 +116,20 @@ def extract_dictation(phrase) -> str:
     return join_punctuation(actions.dictate.parse_words(phrase))
 
 
+def to_formatter_funcs(formatters: str) -> List[Callable]:
+    """Get a list of formatter functions from a string of formatter names."""
+    # TODO: Meaningful error messages
+    return [formatter_functions[name] for name in formatters.split()]
+
+
 def format_contextually(text: str, formatters: List[Callable]) -> ComplexInsert:
     surrounding_text = actions.self.surrounding_text()
     return format_text(text, formatters, surrounding_text)
 
 
 @context.capture(rule="{self.formatters}+")
-def formatters(m) -> List[str]:
-    global formatter_map
-    formatter_words = m.formatters_list
-    return [formatter_map[word] for word in formatter_words]
+def formatters(m) -> str:
+    return " ".join(m.formatters_list)
 
 
 @module.action_class
@@ -132,28 +158,34 @@ class Actions:
         for i in range(len(complex_insert.text_after)):
             actions.key("left")
 
-    def reformat_left(formatters: List[str], number: int) -> None:
+    def reformat_left(formatters: str, number: int) -> None:
         """Reformat a number of words left of the cursor."""
+        formatter_funcs = to_formatter_funcs(formatters)
         text = actions.self.cut_words_left(number)
         surrounding_text = actions.self.surrounding_text()
-        actions.self.insert_complex(reformat_text(text, formatters, surrounding_text))
+        actions.self.insert_complex(
+            reformat_text(text, formatter_funcs, surrounding_text)
+        )
 
-    def insert_formatted(text: str, formatters: List[Callable]) -> None:
-        """Insert ``text``, formatted with ``formatters``."""
-        actions.self.insert_complex(format_contextually(str(text), formatters))
+    def insert_formatted(text: str, formatters: str) -> None:
+        """Insert ``text``, formatted with ``formatters``.
+
+        :param formatters: a space-separated list of formatter names.
+
+        """
+        formatter_funcs = to_formatter_funcs(formatters)
+        actions.self.insert_complex(format_contextually(str(text), formatter_funcs))
+
+    # Convenience functions for very common formatters
 
     def insert_natural(text: str) -> None:
         """Insert text, formatted as natural language."""
-        actions.self.insert_formatted(text, [apply_sentence])
+        actions.self.insert_formatted(text, "sentence")
 
     def insert_capitalized(text: str) -> None:
         """Insert text, formatted as capitalized natural language."""
-        actions.self.insert_formatted(text, [apply_capitalized_sentence])
+        actions.self.insert_formatted(text, "capitalized_sentence")
 
     def insert_lowercase(text: str) -> None:
         """Insert text, formatted as lowercase."""
-        actions.self.insert_formatted(text, [apply_lowercase])
-
-    def insert_spine(text: str) -> None:
-        """Insert text, formatted as spine case."""
-        actions.self.insert_formatted(text, [apply_spine])
+        actions.self.insert_formatted(text, "lowercase")
