896dc7aced3a132de9034e8a91c8a1aa2250a9eb
jcaw <40725916+jcaw@users.noreply.github.com>
2020-04-20 16:32:39 +0100

    Rework formatters to use chunked phrases
    This allows formatted text to be chained with punctuation (and other
    keypresses), making for a much smoother dictation experience.
    
    Also move formatter actions out of the base `text` module and into a new
    `formatting` module.
    
    Bindings using simple dictation and the old formatting system have not been
    updated yet, with the exception of basic formatters. They may be broken until
    they are updated.

--- misc/text.py
+++ misc/text.py
@@ -1,133 +1,13 @@
-import time
-from typing import List, Callable
-import itertools
+from typing import List
 
-from talon import Module, Context, actions
-import talon.clip as clip
-
-from user.utils.formatting import (
-    apply_camel_case,
-    apply_studley_case,
-    apply_snake,
-    apply_spine,
-    apply_dotword,
-    apply_squash,
-    make_apply_delimiter,
-    apply_dunder,
-    apply_uppercase,
-    apply_lowercase,
-    apply_sentence,
-    apply_capitalized_sentence,
-    apply_title,
-    apply_programming_keywords,
-    add_prefix,
-    format_text,
-    reformat_text,
-    ComplexInsert,
-    SurroundingText,
-)
-from user.utils import multi_map, preserve_clipboard
-
-
-# TODO: Quote wrapped stuff. Double/single quotes
-# TODO: Insert punctuation, like brackets.
-
-
-formatter_functions = {
-    "camel": apply_camel_case,
-    "studley": apply_studley_case,
-    "snake": apply_snake,
-    "spine": apply_spine,
-    "dotword": apply_dotword,
-    "squash": apply_squash,
-    "dunder": apply_dunder,
-    "uppercase": apply_uppercase,
-    "lowercase": apply_lowercase,
-    "sentence": apply_sentence,
-    "capitalized_sentence": apply_capitalized_sentence,
-    "title": apply_title,
-    "keywords": apply_programming_keywords,
-    # Language-specific
-    "c_path": make_apply_delimiter("::"),
-    "python_private": add_prefix("_", apply_snake),
-}
-
-
-# Many of these formatters may be chained and applied to a single chunk.
-_chainable_formatters = multi_map(
-    {
-        "camel": "camel",
-        "studley": "studley",
-        "snake": "snake",
-        "spine": "spine",
-        # TODO: Maybe change this command. Dotty?
-        "dotword": "dotword",
-        "squash": "squash",
-        "dunder": "dunder",
-        "upper": "uppercase",
-        # TODO: Maybe don't allow this to be chained?
-        ("lower", "bot"): "lowercase",
-        # Language-specific
-        # TODO: Where is this delimiter used again?
-        "see path": "c_path",  # c path
-        # TODO: Maybe move this into a context-specific list
-        "private": "python_private",
-    }
-)
-# Only one of these formatters can be applied to each chunk.
-_standalone_formatters = {
-    "say": "sentence",
-    "top": "capitalized_sentence",
-    "title": "title",
-    "key": "keywords",
-}
-
-
-# Sanity check
-for spoken_form, formatter_name in itertools.chain(
-    _chainable_formatters.items(), _standalone_formatters.items()
-):
-    assert formatter_name in formatter_functions, f"{spoken_form}: {formatter_name}"
-
-
-# TODO: Wrapped, e.g. parens and string?
-# wrapped_commands = multi_map(
-#     {
-#         ["sing string", "single string"]: apply_speechmarks,
-#         ["dub string", "double string"]: apply_quotemarks,
-#     }
-# )
+from talon import Module, actions
+from user.utils import preserve_clipboard
 
 
 module = Module()
-module.list("chainable_formatters", desc="List of text formatters that can be chained")
-module.list(
-    "standalone_formatters", desc="List of text formatters that cannot be chained"
-)
-
-
-@module.capture(rule="({self.standalone_formatters} | {self.chainable_formatters}+)")
-def formatters(m) -> str:
-    """One or more text formatters, as a space-separated string."""
-    if hasattr(m, "standalone_formatters"):
-        return m.standalone_formatters
-    else:
-        return " ".join(m.chainable_formatters_list)
-
-
-@module.capture(rule="<phrase>")
-def dictation(m) -> str:
-    """Arbitrary dictation."""
-    return extract_dictation(m.phrase)
-
-
-context = Context()
-context.lists["self.chainable_formatters"] = _chainable_formatters
-context.lists["self.standalone_formatters"] = _standalone_formatters
 
 
 def join_punctuation(words: List[str]) -> str:
-    # TODO: Extract to formatter_utils?
     # TODO: Implement proper punctuation joining.
     return " ".join(words)
 
@@ -137,25 +17,14 @@ def extract_dictation(phrase) -> str:
     return join_punctuation(actions.dictate.parse_words(phrase))
 
 
-def to_formatter_funcs(formatters: str) -> List[Callable]:
-    """Get a list of formatter functions from a string of formatter names."""
-    # TODO: Meaningful error messages
-    return [formatter_functions[name] for name in formatters.split()]
-
-
-def format_contextually(text: str, formatters: List[Callable]) -> ComplexInsert:
-    surrounding_text = actions.self.surrounding_text()
-    return format_text(text, formatters, surrounding_text)
+@module.capture(rule="<phrase>")
+def dictation(m) -> str:
+    """Arbitrary dictation."""
+    return extract_dictation(m.phrase)
 
 
 @module.action_class
 class Actions:
-    def surrounding_text() -> SurroundingText:
-        """Get the text on either side of the next insert."""
-        # TODO: Heuristic method for surrounding text in generic case. Try
-        #   using clipboard until we have accessibility interfaces?
-        return None
-
     @preserve_clipboard
     def cut_words_left(number: int) -> str:
         """Cut `number` words left of the cursor."""
@@ -166,42 +35,3 @@ class Actions:
         actions.edit.cut()
         time.sleep(0.1)
         return clip.get()
-
-    def insert_complex(complex_insert: ComplexInsert) -> None:
-        """Input a ComplexInsert into the current program."""
-        actions.insert(complex_insert.insert)
-        actions.insert(complex_insert.text_after)
-        for i in range(len(complex_insert.text_after)):
-            actions.key("left")
-
-    def reformat_left(formatters: str, number: int) -> None:
-        """Reformat a number of words left of the cursor."""
-        formatter_funcs = to_formatter_funcs(formatters)
-        text = actions.self.cut_words_left(number)
-        surrounding_text = actions.self.surrounding_text()
-        actions.self.insert_complex(
-            reformat_text(text, formatter_funcs, surrounding_text)
-        )
-
-    def insert_formatted(text: str, formatters: str) -> None:
-        """Insert ``text``, formatted with ``formatters``.
-
-        :param formatters: a space-separated list of formatter names.
-
-        """
-        formatter_funcs = to_formatter_funcs(formatters)
-        actions.self.insert_complex(format_contextually(str(text), formatter_funcs))
-
-    # Convenience functions for very common formatters
-
-    def insert_natural(text: str) -> None:
-        """Insert text, formatted as natural language."""
-        actions.self.insert_formatted(text, "sentence")
-
-    def insert_capitalized(text: str) -> None:
-        """Insert text, formatted as capitalized natural language."""
-        actions.self.insert_formatted(text, "capitalized_sentence")
-
-    def insert_lowercase(text: str) -> None:
-        """Insert text, formatted as lowercase."""
-        actions.self.insert_formatted(text, "lowercase")
--- talon_files/formatters.talon
+++ talon_files/formatters.talon
@@ -1,8 +1,9 @@
 # Anchor or over means dictation will eat everything after it, but we can still
 # overcome that behavior if necessary.
-<user.formatters> <user.dictation>$:
-    user.insert_formatted(dictation, formatters)
-<user.formatters> <user.dictation> over:
-    user.insert_formatted(dictation, formatters)
+<user.formatted_phrases>$:
+    user.insert_many_formatted(formatted_phrases)
+<user.formatted_phrases> over:
+    user.insert_many_formatted(formatted_phrases)
+
 <user.formatters> <number>:
     user.reformat_left(formatters, number)
