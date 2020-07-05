f5d754486428a42ce37488592db7fd4bb5de84b9
jcaw <toastedjcaw@gmail.com>
2020-07-05 20:53:15 +0100

    Add shell script to install pip requirements

---
+++ install_requirements.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Get this script's folder
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+
+# Install all requirements - useful in Talon beta
+"$DIR/../.venv/bin/pip" install -r "$DIR/requirements.txt" --no-warn-script-location
