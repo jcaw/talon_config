3452ee1d4c64bbe8bbafe50526b1f977a50b9d22
jcaw <toastedjcaw@gmail.com>
2020-07-05 22:23:38 +0100

    Add commands to open & search Talon files in Emacs from anywhere

--- emacs/basic.py
+++ emacs/basic.py
@@ -1,6 +1,12 @@
-from talon import Context, Module, actions
+from typing import Optional
+
+from talon import Context, Module, actions, ui
+from talon_init import TALON_USER
+
+from user.emacs.utils import rpc
 
 key = actions.key
+insert = actions.insert
 user = actions.user
 emacs_command = actions.user.emacs_command
 emacs_fallbacks = actions.user.emacs_fallbacks
@@ -13,6 +19,42 @@ module = Module()
 class Actions:
     def select_number(number: int):
         """Select item ``number``. Meaning of \"select\" depends on context."""
+
+    def open_in_emacs(path: Optional[str] = None) -> None:
+        """Open `path` in Emacs. Required Emacs to initialize as a server.
+
+        Note this requires Emacs to already be open.
+
+        """
+        # TODO: Start Emacs if it's not already running.
+        rpc.call("x-focus-frame", [None])
+        if path:
+            # FIXME: Porthole can't handle unencodable return values
+            rpc.call("voicemacs-find-file", [path])
+
+    def emacs_search_directory(text: Optional[str] = None) -> None:
+        """Search for some `text` in the current project."""
+        emacs_fallbacks(
+            ["spacemacs/helm-dir-smart-do-search", "helm-do-grep-ag", "grep",]
+        )
+        if text:
+            insert(text)
+
+    def emacs_open_talon_user() -> None:
+        """Open the Talon user directory in Emacs."""
+        user.open_in_emacs(str(TALON_USER))
+
+    def emacs_search_talon_user(text: Optional[str] = None) -> None:
+        """Open Emacs & search the Talon user dir for `text`."""
+        user.emacs_open_talon_user()
+        # TODO: Proper lowercase formatting
+        user.emacs_search_directory(text.lower() if text else text)
+
+    def emacs_find_file_talon_user(text: Optional[str] = None) -> None:
+        """Open Emacs & find a file in Talon user w/ name matching `text`."""
+        user.emacs_open_talon_user()
+        user.emacs_command("projectile-find-file")
+        insert(text)
 
 
 context = Context()
---
+++ talon_files/emacs/global_emacs.talon
@@ -0,0 +1,4 @@
+# Emacs-related commands, that are available outside Emacs.
+-
+talon search <user.dictation>: user.emacs_search_talon_user(dictation)
+talon open <user.dictation>:   user.emacs_find_file_talon_user(dictation)
