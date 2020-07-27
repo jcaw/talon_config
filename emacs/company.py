c82ce7447c600f36341c3752772c5fc6f12ead62
jcaw <toastedjcaw@gmail.com>
2020-07-27 20:08:10 +0100

    Fix company number commands
    Some changes in Voicemacs meant they weren't compatible anymore - fix that.

--- emacs/company.py
+++ emacs/company.py
@@ -1,4 +1,7 @@
-from talon import Module
+from talon import Module, actions
+
+user = actions.user
+emacs_prefix_command = actions.user.emacs_prefix_command
 
 from user.emacs.utils import rpc
 
@@ -10,12 +13,18 @@ module = Module()
 class Actions:
     def emacs_company_highlight(number: int) -> None:
         """Move to a specific company candidate."""
-        rpc.call("voicemacs-company-highlight", [number])
+        emacs_prefix_command("voicemacs-company-highlight", number)
 
     def emacs_company_complete(number: int) -> None:
         """Pick & complete a numbered company candidate."""
-        rpc.call("voicemacs-company-complete", [number])
+        # We don't use `voicemacs-company-complete` because it doesn't reliably
+        # give visual feedback.
+        # emacs_prefix_command("voicemacs-company-complete", number)
+        user.emacs_company_highlight(number)
+        # Sleep briefly to ensure there's visual feedback
+        actions.sleep("100ms")
+        user.emacs_command("company-complete")
 
     def emacs_company_show_doc(number: int) -> None:
         """Pop doc for a specific company candidate."""
-        rpc.call("voicemacs-company-pop-doc", [number])
+        emacs_prefix_command("voicemacs-company-pop-doc", number)
