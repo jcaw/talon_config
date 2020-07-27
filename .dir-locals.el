b5f5d123afdf2ef54b9bcaaaf67664595b7f5455
jcaw <toastedjcaw@gmail.com>
2020-07-27 20:25:00 +0100

    Add gross hack to deal with Spacemacs activating Talon's venv
    Without this it keeps popping up Python windows on an idle timer on MS Windows.
    Gah.
    
    This does not seem to be the right way to disable this Spacemacs behavior, but
    it will do for now.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,16 @@
 ((nil . (
+         ;; Spacemacs automatically activates talon's venv, causing all
+         ;; kinds of problems. Just deactivate it.
+         (python-auto-set-local-pyvenv-virtualenv . nil)
          ))
  (python-mode . ((eval . (blacken-mode 1))
                  ;; Some backends cause Flycheck to create temp .py files. Talon
                  ;; will try and auto-load them, causing problems. Blunt
                  ;; solution is to disable it outright.
                  (eval . (flycheck-mode 0))
+                 ;; FIXME: Spacemacs automatically activates talon's venv,
+                 ;; causing all kinds of problems. Just deactivate it.
+                 (eval . (progn
+                           (pyvenv-deactivate)
+                           (pipenv-deactivate)))
                  )))
