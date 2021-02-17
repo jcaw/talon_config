6ac356c119869c7248245e7b17365ed753010da2
jcaw <toastedjcaw@gmail.com>
2021-02-17 15:31:56 +0000

    Kill Talon before pip install on Windows

--- install_requirements.bat
+++ install_requirements.bat
@@ -1,2 +1,4 @@
+rem Kill Talon if running. Need to restart to load the new pip env anyway.
+wmic process where name='talon.exe' delete
 rem Install all requirements - useful in Talon beta
 %~dp0/../.venv/Scripts/pip install -r %~dp0/requirements.txt --no-warn-script-location
