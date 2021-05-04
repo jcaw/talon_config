331bbad2e9110846a2a4157585c7691fb7f6c657
jcaw <toastedjcaw@gmail.com>
2021-05-04 09:32:45 +0100

    Change alphabet

--- misc/keys.py
+++ misc/keys.py
@@ -18,9 +18,16 @@ insert = actions.insert
 default_alphabet = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip"
 # My setup has trouble with some words. Probably my accent.
 modified_alphabet = (
-    default_alphabet.replace("air", "arch").replace("bat", "batch")
-    # .replace("harp", "hip")
-    .replace("zip", "zen")
+    default_alphabet
+    #
+    # .replace("air", "arch")
+    .replace("air", "aim")
+    #
+    # .replace("bat", "batch")
+    #
+    .replace("harp", "hip")
+    #
+    # .replace("zip", "zen")
 )
 # chosen_alphabet = default_alphabet.split(" ")
 chosen_alphabet = modified_alphabet.split(" ")
