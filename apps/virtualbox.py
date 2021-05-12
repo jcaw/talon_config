c66d02d64677b8f3bd4a5a1e58718a3034f9f81c
Jcaw <toastedjcaw@gmail.com>
2021-05-12 10:11:12 +0100

    Set key_hold for virtualbox, don't use keyboard.py

--- apps/virtualbox.py
+++ apps/virtualbox.py
@@ -1,11 +1,10 @@
 from talon import Context
 
-from user.games.utils import switch_input
-
 windows_input_context = Context()
 windows_input_context.matches = r"""
 os: windows
 app: /VirtualBoxVM/
 """
 
-switch_input.switch_to_keyboard_module(windows_input_context)
+windows_input_context.settings["key_hold"] = 50
+windows_input_context.settings["key_wait"] = 20
