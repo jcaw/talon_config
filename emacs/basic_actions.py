4767d6c288b3b929b20aa9facf80b9e3dc38ef25
jcaw <jcaw@DESKTOP-7MH6GKV.localdomain>
2022-10-08 16:42:03 +0100

    Fix some TODOs, add a couple

--- emacs/basic_actions.py
+++ emacs/basic_actions.py
@@ -21,12 +21,18 @@ class UserActions:
 class EditActions:
     def select_all():
         actions.user.emacs_command('mark-whole-buffer')
-        # TODO: Generic Unsorted
-        #
-        # TODO: Fallbacks
+    # TODO: Generic Unsorted
+    #
+    # TODO: Fallbacks
     def zoom_in():  actions.user.emacs_command('voicemacs-increase-text')
     def zoom_out(): actions.user.emacs_command('voicemacs-decrease-text')
     def save():     actions.user.emacs_command('save-buffer')
+    # TODO: save as
+    # def save_as():  actions.user.emacs_command('TODO')
+    # FIXME: Doesn't work, "wrong type argument, listp". RPC problem?
+    # def save_all(): actions.user.emacs_command("save-some-buffers")
+    # Fall back to keypress for now. Should work everywhere anyway, who rebinds
+    # this?
     def save_all(): actions.key('ctrl-x s')
     def undo():     actions.user.emacs_command('undo-fu-only-undo')
     def redo():     actions.user.emacs_command('undo-fu-only-redo')
