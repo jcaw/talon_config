51e9f989596f81a4f627662bc73b89346d5cfba7
jcaw <jcaw@DESKTOP-7MH6GKV.localdomain>
2023-01-16 12:29:54 +0000

    Replace emacs debugger script

---
+++ emacs/modes/debugger.py
@@ -0,0 +1,13 @@
+from talon import Context, actions
+
+context = Context()
+context.matches = """
+tag: user.emacs
+user.emacs-major-mode: debugger-mode
+"""
+
+
+@context.action_class("user")
+class UserActions:
+    def on_pop():
+        actions.self.emacs_command("debugger-step-through")
--- emacs/modes/debugger.talon
+++
@@ -1,6 +0,0 @@
-tag: user.emacs
-user.emacs-major-mode: debugger-mode
--
-# FIXME: Doesn't bind
-actions(on_pop): user.emacs_command("debugger-step-through")
-# TODO: Bind hiss too?
