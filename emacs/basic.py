909cf506c6b98b58ee71a6a2ccc04fb8d31bb748
jcaw <jcaw@DESKTOP-7MH6GKV.localdomain>
2023-06-08 15:35:55 +0100

    Black formatting

--- emacs/basic.py
+++ emacs/basic.py
@@ -195,23 +195,35 @@ class EditActions:
             ["undo-tree-redo", "undo-fu-only-redo"], keypress="ctrl-shift-/"
         )
 
-    def select_all(): actions.user.emacs_command('mark-whole-buffer')
+    def select_all():
+        actions.user.emacs_command("mark-whole-buffer")
+
     # TODO: Generic Unsorted
     #
     # TODO: Fallbacks
-    def zoom_in():  actions.user.emacs_command('voicemacs-increase-text')
-    def zoom_out(): actions.user.emacs_command('voicemacs-decrease-text')
-    def save():     actions.user.emacs_command('save-buffer')
+    def zoom_in():
+        actions.user.emacs_command("voicemacs-increase-text")
+
+    def zoom_out():
+        actions.user.emacs_command("voicemacs-decrease-text")
+
+    def save():
+        actions.user.emacs_command("save-buffer")
+
     # TODO: save as
     # def save_as():  actions.user.emacs_command('TODO')
     # FIXME: Doesn't work, "wrong type argument, listp". RPC problem?
     # def save_all(): actions.user.emacs_command("save-some-buffers")
     # Fall back to keypress for now. Should work everywhere anyway, who rebinds
     # this?
-    def save_all(): actions.key('ctrl-x s')
-    def undo():     actions.user.emacs_command('undo-fu-only-undo')
-    def redo():     actions.user.emacs_command('undo-fu-only-redo')
+    def save_all():
+        actions.key("ctrl-x s")
 
+    def undo():
+        actions.user.emacs_command("undo-fu-only-undo")
+
+    def redo():
+        actions.user.emacs_command("undo-fu-only-redo")
 
 
 @context.action_class("user")
@@ -219,9 +231,11 @@ class UserActions:
     # Fundamental Commands
 
     # "c-g" is the interrupt command. It's hardcoded in C - can't remap it.
-    def cancel() -> None:         actions.key('ctrl-g')
-    def toggle_comment() -> None: actions.user.emacs_command('spacemacs/comment-or-uncomment-lines')
+    def cancel() -> None:
+        actions.key("ctrl-g")
 
+    def toggle_comment() -> None:
+        actions.user.emacs_command("spacemacs/comment-or-uncomment-lines")
 
     def open_file() -> None:
         emacs_fallbacks(
