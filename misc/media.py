273ebbc7e3a6c68f8e968284e22f29d254862870
Jcaw <toastedjcaw@gmail.com>
2024-04-16 16:49:16 -0400

    Media control

--- misc/media.py
+++ misc/media.py
@@ -1,4 +1,7 @@
-from talon import Module
+from talon import Module, actions, cron
+
+key = actions.key
+
 
 module = Module()
 
@@ -7,3 +10,31 @@ module = Module()
 class Actions:
     def set_volume(percent: int) -> None:
         """Set the volume to a specific percentage value."""
+
+
+def bind():
+    try:
+        play_pause = (lambda: key("play_pause"), "Play/Pause")
+        actions.user.vimfinity_bind_keys(
+            {
+                "x": "Media",
+                "x p": (lambda: key("volup"), "Volume Up"),
+                "x p": (lambda: key("voldown"), "Volume Down"),
+                "x m": (lambda: key("mute"), "Volume Mute"),
+                "x p": play_pause,
+                # TODO: Pure play button - but it doesn't work to control Firefox.
+                "x s": (lambda: key("stop"), "Stop"),
+                "x ]": (lambda: key("next"), "Next Track"),
+                "x [": (lambda: key("prev"), "Previous Track"),
+                # Also bind pause to space for max convenience.
+                "x space": play_pause,
+                # This matches the keychron
+                # "f8": play_pause,
+            }
+        )
+    except KeyError:
+        print("Failed to register media key sequences. Retrying in 1s.")
+        cron.after("1s", bind)
+
+
+cron.after("50ms", bind)
--- misc/media.talon
+++ misc/media.talon
@@ -2,10 +2,15 @@
 volume up: key(volup)
 volume down: key(voldown)
 volume (mute | unmute): key(mute)
-play track: key(play)
-pause track: key(pause)
+# play track: key(play)
+play track: key(play_pause)
+pause track: key(play_pause)
+stop track: key(stop)
 (next | neck) track: key(next)
 (last | larse) track: key(prev)
+
+# rewind: key(rewind)
+# fast forward: key(fast_forward)
 
 # TODO: Brightness commands
 # brightness up | increase brightness: key(brightup)
