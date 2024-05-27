cf2b0170dfdea3c1f8cb81c1dfebacdcf69d88bc
Jcaw <toastedjcaw@gmail.com>
2024-05-27 16:15:32 -0400

    Commands to automatically download videos

--- websites/youtube.py
+++ websites/youtube.py
@@ -1,6 +1,15 @@
 import re
 
-from talon import Module, Context, actions, clip, cron
+from talon import Module, Context, actions, clip, cron, ui
+
+from user.plugins.accessibility_automator.accessibility_automator import (
+    AutomationOverlay,
+    Spec,
+    ElementNotFoundError,
+)
+
+user = actions.user
+key = actions.key
 
 
 module = Module()
@@ -10,6 +19,24 @@ module.tag(
 )
 
 
+def add_video_to_yt_dlg():
+    # The yt-dlg GUI seems to have no heirarchy - every element is top-level so we search for elements from top-level.
+    url = user.browser_address_backup()
+    started_new = user.switch_or_start("yt-dlg", focus_name="YouTube Downloader GUI")
+    if not started_new:
+        # Focussed existing instance - so we need to ensure it's in the correct
+        # state to receive input.
+        actions.sleep("200ms")
+        if ui.active_window().title == "Info":
+            key("enter")
+        user.automator_click_element([Spec(name="Enter URLs below", class_name="Edit")])
+    user.paste_insert(url)
+    # HACK: It's faster to just backtab once, even though that might be janky.
+    key("shift-tab")
+    key("enter")
+    # user.automator_click_element([Spec(name="Add", class_name="Button")])
+
+
 @module.action_class
 class Actions:
     def youtube_toggle_short() -> None:
@@ -17,7 +44,7 @@ class Actions:
         # Get the URL
         # HACK: Not implemented at time of writing - instead do it manually.
         # url = actions.browser.address()
-        url = actions.user.browser_address_backup()
+        url = user.browser_address_backup()
 
         short_regex = r"www.youtube.com\/shorts/"
         video_regex = r"www.youtube.com\/watch\?v\="
@@ -33,7 +60,32 @@ class Actions:
         # actions.browser.go(current_url)
         clip.set_text(new_url)
         actions.edit.paste()
-        actions.key("enter")
+        key("enter")
+
+    def youtube_queue_with_yt_dlg():
+        """Download the current YouTube video using `yt-dlg`."""
+        with AutomationOverlay():
+            original_window = ui.active_window()
+
+            add_video_to_yt_dlg()
+
+            original_window.focus()
+
+    def youtube_download_with_yt_dlg():
+        """Download the current YouTube video using `yt-dlg`, to the default yt-dlg folder."""
+        with AutomationOverlay():
+            original_window = ui.active_window()
+
+            add_video_to_yt_dlg()
+
+            # HACK: The download button is labelled extremely generically
+            #  (name="", class_name="Button"), so we can't really get it with
+            #  the UI automator. It's labelled too ambiguously. Instead we'll
+            #  try tabbing to it.
+            key("shift-tab:3")
+            key("enter")
+
+            original_window.focus()
 
 
 context = Context()
@@ -48,6 +100,14 @@ def bind_vimfinity_keys():
     actions.user.vimfinity_bind_keys(
         {
             "p s": (actions.user.youtube_toggle_short, "Switch between Video & Short"),
+            "p d": (
+                actions.user.youtube_download_with_yt_dlg,
+                "Download Video With yt-dlg",
+            ),
+            "p q": (
+                actions.user.youtube_queue_with_yt_dlg,
+                "Add Video to yt-dlg Queue",
+            ),
         },
         context,
     )
