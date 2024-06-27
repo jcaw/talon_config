ae9a9ed8a4486d8750d2de9d9968ca15510085f1
Jcaw <toastedjcaw@gmail.com>
2024-06-26 21:08:24 -0400

    Retry binding

--- websites/unreal_engine_documentation.py
+++ websites/unreal_engine_documentation.py
@@ -57,12 +57,16 @@ context.tags = ["user.unreal_online_docs"]
 
 
 def bind_vimfinity_keys():
-    actions.user.vimfinity_bind_keys(
-        {
-            "p c": actions.user.unreal_docs_switch_to_current_version,
-        },
-        context,
-    )
+    try:
+        actions.user.vimfinity_bind_keys(
+            {
+                "p c": actions.user.unreal_docs_switch_to_current_version,
+            },
+            context,
+        )
+    except IndexError:
+        print("Failed to bind Unreal Docs vimfinity keys. Retrying in 1s.")
+        cron.after("1s", bind_vimfinity_keys)
 
 
 cron.after("50ms", bind_vimfinity_keys)
