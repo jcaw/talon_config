f02d5a031951d9a9133b5b16fab49ad7d8389d42
jcaw <toastedjcaw@gmail.com>
2025-04-06 01:21:52 +0100

    Shortcuts for setting volume levels

--- misc/media.py
+++ misc/media.py
@@ -2,7 +2,7 @@ from talon import Module, actions, cron
 from user.plugins.vimfinity.vimfinity import vimfinity_bind_keys
 
 key = actions.key
-
+set_volume = actions.self.set_volume
 
 module = Module()
 
@@ -29,5 +29,18 @@ vimfinity_bind_keys(
         "x space": play_pause,
         # This matches the keychron
         # "f8": play_pause,
+        "x v": "Volume",
+        "x v `": (lambda: set_volume(0), "0%"),
+        "x v 0": (lambda: set_volume(0), "0%"),
+        "x v 1": (lambda: set_volume(10), "10%"),
+        "x v 2": (lambda: set_volume(20), "20%"),
+        "x v 3": (lambda: set_volume(30), "30%"),
+        "x v 4": (lambda: set_volume(40), "40%"),
+        "x v 5": (lambda: set_volume(50), "50%"),
+        "x v 6": (lambda: set_volume(60), "60%"),
+        "x v 7": (lambda: set_volume(70), "70%"),
+        "x v 8": (lambda: set_volume(80), "80%"),
+        "x v 9": (lambda: set_volume(90), "90%"),
+        "x v =": (lambda: set_volume(100), "100%"),
     }
 )
